2e28f46d6eed2a7ec829a84072e80ea6ead316cc
Hamid Kamalov <hamid_kamalov@mail.ru>
2022-11-17 00:51:23 +0900

    COunt_4

--- count_all.py
+++ count_all.py
@@ -1,5 +1,6 @@
 def count_all(txt):
     """
+
     Given a function that takes a string and calculates the number of letters and digits within it.
     Return the result in a dictionary.
     Args:
@@ -10,12 +11,12 @@ def count_all(txt):
     letter=0
     digits=0
     idx=0
-    while idx<len(txt):
+    for idx in range(len(txt)):
         if txt[idx].isalpha():
             letter+=1
         if txt[idx].isdigit():
             digits+=1
-        idx+=1
+        idx+=1 
     count={'LETTERS=':letter,'DIGITS=':digits}
     return count
 print(count_all("Asus pro max 3000"))

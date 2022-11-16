61b79f271211e634e24155ad668b5d6d5eadf02b
Hamid Kamalov <hamid_kamalov@mail.ru>
2022-11-17 00:29:06 +0900

    2nd try

--- first_item.py
+++ first_item.py
@@ -3,6 +3,6 @@ def first_item():
     Given a dictionary, Return first item value.
     """
     data = {'a': 1, 'b': 2}
-    return print(data['a'])
+    return data['a']
 
-first_item()
+print(first_item())

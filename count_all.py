31325c00ba52212e53877e200cee803ca6ec2b0e
Hamid Kamalov <hamid_kamalov@mail.ru>
2022-11-17 00:21:49 +0900

    Count

--- count_all.py
+++ count_all.py
@@ -7,4 +7,15 @@ def count_all(txt):
     Returns:
         dict: dictionary with letters and digits
     """
-    return
+    letter=0
+    digits=0
+    idx=0
+    while idx<len(txt):
+        if txt[idx].isalpha():
+            letter+=1
+        if txt[idx].isdigit():
+            digits+=1
+        idx+=1
+    count={letter:digits}
+    return count
+print(count_all("Asus pro max 3000"))
